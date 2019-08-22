9506e93fca4c91020348474e13e6ab997d8f2648
techygamerkid <54126919+techygamerkid@users.noreply.github.com>
2019-08-22 15:58:31 -0700

    update

--- landscape/script.js
+++ landscape/script.js
@@ -1,12 +1,14 @@
-if (screen.width >= 699 && screen.width <= 900) {
-document.location = "/landscape/index.html";
-}
-if (screen.width <= 699) {
-document.location = "/mobile/index.html";
-}
+
+// When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
+
+// Get the header
 var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
 var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
