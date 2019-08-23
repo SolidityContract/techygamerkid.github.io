4fd7186363104c24f28468189b40b6c4a91285f1
techygamerkid <54126919+techygamerkid@users.noreply.github.com>
2019-08-22 18:50:24 -0700

    darkmode test

--- script.js
+++ script.js
@@ -14,39 +14,3 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
-if (document.cookie == "theme=Dark") {
-  var style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet.insertRule('body {background-color: #242424}');
-  style.sheet.insertRule('.header {background-color: #242424}');
-  style.sheet.insertRule('p {color: #FFFFFF}');
-  style.sheet.insertRule('h1 {color: #FFFFFF}');
-  style.sheet.insertRule('h2 {color: #FFFFFF}');
-}
-if (document.cookie == "theme=Light") {
-  var style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet.insertRule('body {background-color: #FFFFFF}');
-  style.sheet.insertRule('.header {background-color: #FFFFFF}');
-  style.sheet.insertRule('p {color: #000000}');
-  style.sheet.insertRule('h1 {color: #000000}');
-  style.sheet.insertRule('h2 {color: #000000}');
-}
-if (document.cookie == "theme=Carbon") {
-  var style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet.insertRule('body {background-image: url("carbon.png")}');
-  style.sheet.insertRule('.header {background-image: url("carbon.png")}');
-  style.sheet.insertRule('p {color: #FFFFFF}');
-  style.sheet.insertRule('h1 {color: #FFFFFF}');
-  style.sheet.insertRule('h2 {color: #FFFFFF}');
-}
-if (document.cookie == "theme=Blend") {
-  var style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet.insertRule('body {background-color: #242424}');
-  style.sheet.insertRule('.header {background-image: url("carbon.png")}');
-  style.sheet.insertRule('p {color: #FFFFFF}');
-  style.sheet.insertRule('h1 {color: #FFFFFF}');
-  style.sheet.insertRule('h2 {color: #FFFFFF}');
-}
